8785667e4bc18931c20efad93449756fb0b9519a
SupItsMike <github@supitsmike.dev>
2025-02-10 15:51:24 -0500

    Update SQL script for users table.

--- SQLScripts/table_users.sql
+++ SQLScripts/table_users.sql
@@ -7,6 +7,7 @@ CREATE TABLE IF NOT EXISTS public.users
     user_id numeric(19,0) NOT NULL,
     balance money NOT NULL DEFAULT 100,
     banned boolean NOT NULL DEFAULT false,
+    daily_cooldown timestamp without time zone,
     CONSTRAINT users_pkey PRIMARY KEY (user_id),
     CONSTRAINT users_user_id_key UNIQUE (user_id)
 )
