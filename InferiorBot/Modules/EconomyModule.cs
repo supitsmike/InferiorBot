4540a7d65484b43a323f3e031ce1e6033041c2b2
SupItsMike <github@supitsmike.dev>
2025-02-17 22:23:45 -0500

    Cleanup `daily` command.

--- InferiorBot/Modules/EconomyModule.cs
+++ InferiorBot/Modules/EconomyModule.cs
@@ -1,7 +1,9 @@
 ﻿using Discord;
 using Discord.Interactions;
 using InferiorBot.Classes;
+using InferiorBot.Extensions;
 using Infrastructure.InferiorBot;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 
 namespace InferiorBot.Modules
@@ -14,21 +16,29 @@ namespace InferiorBot.Modules
         [SlashCommand("daily", "Claim your daily bonus.")]
         public async Task DailyBonus()
         {
-            if (UserData.DailyCooldown != null && UserData.DailyCooldown > DateTime.Now)
+            if (UserData.DailyCooldown.HasValue && UserData.DailyCooldown > DateTime.Now)
             {
                 await RespondAsync(embed: new EmbedBuilder
                 {
                     Title = "Daily bonus not ready",
-                    Description = $":hourglass_flowing_sand: You have already collected your daily bonus.{Environment.NewLine}Try again {Format.Bold(DiscordFormatter.Timestamp(UserData.DailyCooldown.Value))}",
+                    Description = $"""
+                                   :hourglass_flowing_sand: You have already collected your daily bonus.
+                                   Try again {Format.Bold(DiscordFormatter.Timestamp(UserData.DailyCooldown.Value))}
+                                   """,
                     Color = Color.DarkRed
                 }.Build(), ephemeral: true);
                 return;
             }
 
+            UserData.DailyCount++;
+            UserData.DailyStreak =
+                UserData.DailyCooldown.HasValue && UserData.DailyCooldown.Value.Date == DateTime.Now.Date
+                    ? UserData.DailyStreak + 1
+                    : 1;
+            UserData.DailyCooldown = DateTime.Now.AddDays(1);
+
             var dailyBonus = Configuration.GetValue<decimal>("Settings:DailyBonus");
             UserData.Balance += dailyBonus;
-            UserData.DailyCooldown = DateTime.Now.AddDays(1);
-            UserData.DailyStreak++;
 
             if (!_context.ChangeTracker.HasChanges())
             {
