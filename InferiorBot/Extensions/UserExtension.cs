498beec9a49797a0d22b8f56b3f9c8ca4d3166a8
SupItsMike <github@supitsmike.dev>
2025-02-17 22:24:16 -0500

    Add `select-job` and `work` command

---
+++ InferiorBot/Extensions/UserExtension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Infrastructure.InferiorBot;
+
+namespace InferiorBot.Extensions
+{
+    public static class UserExtension
+    {
+        public static void WriteAuditLog(this InferiorBotContext context, decimal userId, string tableName, string columnName)
+        {
+            context.AuditLogs.AddAsync(new AuditLog
+            {
+                LogId = Guid.NewGuid(),
+                UserId = userId,
+                TableName = "",
+                ColumnName = "",
+                NewData = "",
+                PreviousData = ""
+            });
+        }
+    }
+}
--- InferiorBot/Modules/EconomyModule.cs
+++ InferiorBot/Modules/EconomyModule.cs
@@ -58,5 +58,97 @@ namespace InferiorBot.Modules
                 }
             }.Build(), ephemeral: true);
         }
+
+        [SlashCommand("select-job", "Claim your daily bonus.")]
+        public async Task SelectJob()
+        {
+            var jobList = await _context.Jobs
+                .Select(job => new SelectMenuOptionBuilder(job.JobTitle, $"{job.JobId}",
+                    $"Pay rate: {job.PayMin:$#,##0;-$#,##0} to {job.PayMax:$#,##0;-$#,##0} | Success rate {job.Probability:P0}",
+                    null, null))
+                .ToListAsync();
+
+            var components = new ComponentBuilder()
+                .WithSelectMenu("select-job-menu", jobList, "Select Job...")
+                .Build();
+            
+            await RespondAsync("Please select a job: ", components: components, ephemeral: true);
+        }
+
+        [ComponentInteraction("select-job-menu")]
+        public async Task SelectJobMenu()
+        {
+            if (Context.Interaction is not IComponentInteraction componentInteraction) return;
+            
+            var jobId = Convert.ToInt32(componentInteraction.Data.Values.First());
+            var selectedJob = await _context.Jobs.Where(job => job.JobId == jobId).FirstOrDefaultAsync();
+            if (selectedJob == null) return;
+
+            UserData.JobId = selectedJob.JobId;
+
+            if (!_context.ChangeTracker.HasChanges())
+            {
+                await RespondAsync("Failed to update user.", ephemeral: true);
+                return;
+            }
+            await _context.SaveChangesAsync();
+
+            await RespondAsync($"Your job is now a {Format.Bold(selectedJob.JobTitle)}!", ephemeral: true);
+        }
+
+
+        [SlashCommand("work", "Work to earn money.")]
+        public async Task DoWork()
+        {
+            var selectedJob = await _context.Jobs.Where(job => job.JobId == UserData.JobId).FirstOrDefaultAsync();
+            if (selectedJob == null)
+            {
+                var selectJob = Commands.FirstOrDefault(x => x.Name == "select-job");
+                await RespondAsync(
+                    $"You must select a job first. Do {(selectJob != null ? DiscordFormatter.Mention(selectJob) : "/select-job")} first.",
+                    ephemeral: true);
+                return;
+            }
+
+            if (UserData.WorkCooldown.HasValue && UserData.WorkCooldown.Value > DateTime.Now)
+            {
+                await RespondAsync(embed: new EmbedBuilder
+                {
+                    Title = "Cannot work again yet",
+                    Description =
+                        $":hourglass_flowing_sand: You can work again {Format.Bold(DiscordFormatter.Timestamp(UserData.WorkCooldown.Value))}",
+                    Color = Color.DarkRed
+                }.Build(), ephemeral: true);
+                return;
+            }
+
+            var (amount, success) = selectedJob.GetPayAmount();
+            UserData.Balance += amount;
+
+            UserData.WorkCooldown = DateTime.Now.AddMinutes(selectedJob.Cooldown);
+            UserData.WorkCount++;
+
+            if (!_context.ChangeTracker.HasChanges())
+            {
+                await RespondAsync("Failed to update user.", ephemeral: true);
+                return;
+            }
+            await _context.SaveChangesAsync();
+
+            await RespondAsync(embed: new EmbedBuilder
+            {
+                Title = "Work Reward",
+                Description = $"""
+                               {(success == false ? "You failed the job." : null)}
+
+                               You {(success == false ? "lost" : "earned")} {Format.Bold($"{amount:$#,##0.00;$#,##0.00}")} while working.
+                               """,
+                Color = Color.DarkGreen,
+                Footer = new EmbedFooterBuilder
+                {
+                    Text = $"New balance is {UserData.Balance:C}"
+                }
+            }.Build(), ephemeral: true);
+        }
     }
 }
