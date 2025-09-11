b05d5b5fc10f1c5da2cb2e591df0c114cf3c2bfc
SupItsMike <github@supitsmike.dev>
2025-09-11 12:36:32 -0400

    Add a `ToObject` extension on the string object.

--- InferiorBot/Extensions/StringExtension.cs
+++ InferiorBot/Extensions/StringExtension.cs
@@ -1,10 +1,17 @@
-﻿namespace InferiorBot.Extensions
+﻿using Newtonsoft.Json;
+
+namespace InferiorBot.Extensions
 {
     public static class StringExtension
     {
         public static bool IsValidUrl(this string s)
         {
             return Uri.IsWellFormedUriString(s, UriKind.Absolute);
+        }
+
+        public static T? ToObject<T>(this string s)
+        {
+            return string.IsNullOrWhiteSpace(s) ? default : JsonConvert.DeserializeObject<T>(s);
         }
     }
 }
