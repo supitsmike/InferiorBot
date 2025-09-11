566f0f3faf89594d063dc19ba0bdb0b7bf67a9a5
SupItsMike <github@supitsmike.dev>
2025-09-11 13:03:16 -0400

    Cleanup coinflip command.

--- InferiorBot/Modules/GamblingModule.cs
+++ InferiorBot/Modules/GamblingModule.cs
@@ -22,43 +22,31 @@ namespace InferiorBot.Modules
         [SlashCommand("coinflip", "Flip a coin for a chance to double your money.")]
         public async Task CoinFlip([Summary(description: "heads or tails?")] CoinFlipEnum selection, [Summary(description: "How much money would you like to bet? (all is also valid)")] string? amount = null)
         {
-            var sides = new[] { CoinFlipEnum.Heads.ToString(), CoinFlipEnum.Tails.ToString() };
+            var sides = new[] { nameof(CoinFlipEnum.Heads), nameof(CoinFlipEnum.Tails) };
             var index = NumericRandomizer.GenerateRandomNumber(sides.Length);
             var result = sides[index] == selection.ToString();
 
-            Embed embed;
-            if (!string.IsNullOrEmpty(amount))
+            var embedBuilder = new EmbedBuilder
             {
-                switch (UserData.Balance)
+                Title = result ? "WINNER" : "LOSER",
+                Color = result ? Color.DarkGreen : Color.DarkRed,
+                ThumbnailUrl = $"attachment://{sides[index]}.png",
+                Author = new EmbedAuthorBuilder
                 {
-                    case 0:
-                        await RespondAsync(":x: You have no money!", ephemeral: true);
-                        return;
-                    case < 0:
-                        await RespondAsync(":x: You are already in dept!", ephemeral: true);
-                        return;
+                    IconUrl = AuthorIconUrl,
+                    Name = AuthorName
                 }
+            };
 
-                amount = amount.ToLower();
-                if (amount != "all" && !decimal.TryParse(amount, out _)) amount = "0";
-                if (amount.Contains('.') && amount.Split('.')[1].Length > 2)
+            var (betAmount, error) = UserData.GetBetAmount(amount);
+            if (!string.IsNullOrWhiteSpace(error))
                 {
-                    await RespondAsync(":x: Invalid bet amount!", ephemeral: true);
+                await RespondAsync(error, ephemeral: true);
                     return;
                 }
 
-                var betAmount = amount == "all" ? UserData.Balance : Convert.ToDecimal(amount);
-                if (betAmount > UserData.Balance)
+            if (betAmount > 0m)
                 {
-                    await RespondAsync(":x: You can't bet more money than you have!", ephemeral: true);
-                    return;
-                }
-                if (betAmount <= 0)
-                {
-                    await RespondAsync(":x: You can't bet nothing!", ephemeral: true);
-                    return;
-                }
-
                 if (result)
                 {
                     UserData.WonMoney(betAmount);
@@ -77,49 +65,29 @@ namespace InferiorBot.Modules
                 }
                 await _context.SaveChangesAsync();
 
-                embed = new EmbedBuilder
-                {
-                    Title = result ? "WINNER" : "LOSER",
-                    Description = $"""
+                embedBuilder.Description = $"""
                                    You bet {Format.Bold($"{betAmount:C}")} on {selection}.
                                    It was {sides[index]}.
-                                   """,
-                    Color = result ? Color.DarkGreen : Color.DarkRed,
-                    ThumbnailUrl = $"attachment://{sides[index]}.png",
-                    Author = new EmbedAuthorBuilder
-                    {
-                        IconUrl = AuthorIconUrl,
-                        Name = AuthorName
-                    },
-                    Footer = new EmbedFooterBuilder
+                                            """;
+                embedBuilder.Footer = new EmbedFooterBuilder
                     {
                         Text = $"New balance is {UserData.Balance:C}"
+                };
                     }
-                }.Build();
-            }
             else
             {
-                embed = new EmbedBuilder
-                {
-                    Title = result ? "WINNER" : "LOSER",
-                    Description = $"""
+                embedBuilder.Description = $"""
                                    You guessed {selection}.
                                    It was {sides[index]}.
-                                   """,
-                    Color = result ? Color.DarkGreen : Color.DarkRed,
-                    ThumbnailUrl = $"attachment://{sides[index]}.png",
-                    Author = new EmbedAuthorBuilder
-                    {
-                        IconUrl = AuthorIconUrl,
-                        Name = AuthorName
+                                            """;
                     }
-                }.Build();
-            }
             
-            var image = sides[index] == CoinFlipEnum.Heads.ToString()
+            var image = sides[index] == nameof(CoinFlipEnum.Heads)
                 ? Properties.Resources.penny_heads
                 : Properties.Resources.penny_tails;
             var file = new FileAttachment(new MemoryStream(image), $"{sides[index]}.png");
+            
+            var embed = embedBuilder.Build();
             await RespondWithFileAsync(file, embed: embed);
         }
 
