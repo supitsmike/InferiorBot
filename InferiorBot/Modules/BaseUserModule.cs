eef49beb613da337d434bfff649d26dcb6dcbdd6
SupItsMike <github@supitsmike.dev>
2025-09-11 12:38:55 -0400

    Cleanup ActiveGames object in base user module.

--- InferiorBot/Modules/BaseUserModule.cs
+++ InferiorBot/Modules/BaseUserModule.cs
@@ -1,11 +1,12 @@
 ﻿using Discord;
-﻿using Discord.Interactions;
+using Discord.Interactions;
 using Discord.WebSocket;
 using InferiorBot.Extensions;
 using Infrastructure.InferiorBot;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using InferiorGame = Infrastructure.InferiorBot.Game;
 
 namespace InferiorBot.Modules
 {
@@ -20,7 +21,7 @@ namespace InferiorBot.Modules
 
         protected Guild GuildData = null!;
         protected User UserData = null!;
-        protected List<Game> ActiveGames = null!;
+        protected List<InferiorGame> ActiveGames = null!;
         protected List<ConvertedUrl> ConvertedUrls = null!;
 
         public override async Task BeforeExecuteAsync(ICommandInfo command)
@@ -32,12 +33,13 @@ namespace InferiorBot.Modules
             AuthorName = $"{Context.User.Username}{(Context.User.Discriminator != "0000" ? $"#{Context.User.Discriminator}" : string.Empty)}";
             AuthorIconUrl = Context.User.GetDisplayAvatarUrl() ?? Context.User.GetAvatarUrl() ?? Context.User.GetDefaultAvatarUrl();
 
+            var userId = Convert.ToString(Context.User.Id);
             var guildId = Convert.ToString(Context.Guild.Id);
             var channelId = Convert.ToString(Context.Channel.Id);
 
             GuildData = await Context.Guild.GetGuildDataAsync(context);
             UserData = await Context.User.GetUserDataAsync(context, services);
-            ActiveGames = await context.Games.Include(x => x.GameUsers).Where(x => x.GuildId == guildId).ToListAsync();
+            ActiveGames = await context.Games.Include(x => x.GameUsers).Where(x => x.UserId == userId || x.GuildId == guildId).ToListAsync();
             ConvertedUrls = await context.ConvertedUrls.Where(x => x.Guild.GuildId == guildId && x.ChannelId == channelId).ToListAsync();
 
             await base.BeforeExecuteAsync(command);
