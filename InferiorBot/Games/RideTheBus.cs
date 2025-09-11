40c772d2760a30670f5a388ea3146428c0eab1d2
SupItsMike <github@supitsmike.dev>
2025-09-11 15:57:34 -0400

    Create the Ride the Bus game class.

---
+++ InferiorBot/Games/RideTheBus.cs
@@ -0,0 +1,11 @@
+﻿using InferiorBot.Classes;
+
+namespace InferiorBot.Games
+{
+    public class RideTheBus(List<Card> cards) : BaseGame
+    {
+        public List<Card> Cards { get; set; } = cards;
+        public bool[] RevealedCards { get; set; } = [false, false, false, false];
+        public decimal BetAmount { get; set; }
+    }
+}
