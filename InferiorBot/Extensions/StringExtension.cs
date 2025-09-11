99a260feccf2e4e4b7fc7990281cf0b0bf280cec
SupItsMike <github@supitsmike.dev>
2025-09-11 15:55:28 -0400

    Add a simple `ToTitleCase` extension off string.

--- InferiorBot/Extensions/StringExtension.cs
+++ InferiorBot/Extensions/StringExtension.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using System.Globalization;
 
 namespace InferiorBot.Extensions
 {
@@ -6,12 +7,17 @@ namespace InferiorBot.Extensions
     {
         public static bool IsValidUrl(this string s)
         {
-            return Uri.IsWellFormedUriString(s, UriKind.Absolute);
+            return !string.IsNullOrWhiteSpace(s) && Uri.IsWellFormedUriString(s, UriKind.Absolute);
         }
 
         public static T? ToObject<T>(this string s)
         {
             return string.IsNullOrWhiteSpace(s) ? default : JsonConvert.DeserializeObject<T>(s);
+        }
+
+        public static string ToTitleCase(this string s, bool everyWord = false)
+        {
+            return string.IsNullOrWhiteSpace(s) ? s : CultureInfo.CurrentCulture.TextInfo.ToTitleCase(s);
         }
     }
 }
